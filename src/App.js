a2838c7304bd49d1c9667d0caa882e1ff9de61f8
ravi ranjan <ravikumarranjan89555@gmail.com>
2021-06-15 14:27:05 +0530

    learn basic and props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Name from "./Name";
 
 function App() {
+  const name = [
+    {
+      name : 'ravi'
+    },
+    {
+      name : 'ravi1'
+    },
+    {
+      name : 'ravi2'
+    }
+  ]
+
+  const output = name.map( val => {
+    return <Name name={val.name} />
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     {output}
+    </>
   );
 }
 
---
+++ src/Name.js
@@ -0,0 +1,7 @@
+const Name = ({name}) => {
+   return <>
+  <h1> this is {name}</h1>
+    </>
+}
+
+export default Name;
